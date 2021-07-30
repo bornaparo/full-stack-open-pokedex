fe217289e8390d4e8c59aad51154cfa45c76d8d7
Borna <bornapar@gmail.com>
2021-07-30 12:27:57 +0200

    cao

---
+++ cypress/integration/my_tests/my_test.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
+
+describe('Pikachu', function() {
+  it("open pikachu", function() {
+    // cy.visit('http://localhost:5000/pokemon/pikachu')
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu').click()
+    cy.contains('lightning rod')
+  })
+})
