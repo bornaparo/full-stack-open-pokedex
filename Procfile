1d0e218d1c7ba1aa3544e60d468fcb60e9a59e42
Borna <bornapar@gmail.com>
2021-08-02 11:47:13 +0200

    zadnji valjda

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run start-proddddddddddddddddd
+web: npm run start-prod
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
+app.get('/health', (req, res) => { //za healthcheck, ako aplikacija radi nakon deploymenta 
   res.send('ok')
 })
 
